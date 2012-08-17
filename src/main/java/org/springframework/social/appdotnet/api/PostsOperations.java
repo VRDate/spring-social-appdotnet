fc431a7126ab9c848851caf39ce7971f6ec4e2ed
Arik Galansky <arik.galansky@gmail.com>
2012-08-17 14:40:58 +0300

    posts operation - added all get operations

--- src/main/java/org/springframework/social/appdotnet/api/PostsOperations.java
+++ src/main/java/org/springframework/social/appdotnet/api/PostsOperations.java
@@ -8,7 +8,22 @@ import java.util.List;
  * @author Arik Galansky
  */
 public interface PostsOperations {
+
     public ADNPost get(String id);
 
-    public List<ADNPost> getStream(String userId);
+    public void delete(String id);
+
+    public List<ADNPost> getPersonalStream();
+
+    public List<ADNPost> getUserCreated(String userId);
+
+    public List<ADNPost> getUserMentions(String userId);
+
+    public List<ADNPost> getGlobalStream();
+
+    public List<ADNPost> getHashtagStream(String hashtag);
+
+    public List<ADNPost> getPostReplies(String id);
+
+
 }
--- src/main/java/org/springframework/social/appdotnet/api/impl/PostsTemplate.java
+++ src/main/java/org/springframework/social/appdotnet/api/impl/PostsTemplate.java
@@ -3,6 +3,7 @@ package org.springframework.social.appdotnet.api.impl;
 import org.springframework.social.appdotnet.api.PostsOperations;
 import org.springframework.social.appdotnet.api.data.post.ADNPost;
 import org.springframework.social.appdotnet.api.data.post.ADNPostsList;
+import org.springframework.social.support.URIBuilder;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -10,10 +11,17 @@ import java.util.List;
 /**
  * @author Arik Galansky
  */
+//TODO Arikg: add general parameters to all streams
+//TODO Arikg: create post + edit AppdotnetAdapter to use it
 public class PostsTemplate extends AbstractAppdotnetOperations implements PostsOperations {
+
+    // TODO Arikg: this is a bad solution, the endpoint should probably change
+    private String userPostsBaseURl;
 
     public PostsTemplate(String accessToken, RestTemplate restTemplate) {
         super(accessToken, restTemplate, "posts", VERSION_0);
+        this.userPostsBaseURl = new StringBuilder().append("https://alpha-api.app.net/stream/")
+                .append(VERSION_0).append("/").append("users").append("/").toString();
     }
 
     @Override
@@ -22,7 +30,42 @@ public class PostsTemplate extends AbstractAppdotnetOperations implements PostsO
     }
 
     @Override
-    public List<ADNPost> getStream(String userId) {
+    public void delete(String id) {
+        restTemplate.delete(buildUri(id));
+    }
+
+    @Override
+    public List<ADNPost> getPersonalStream() {
         return restTemplate.getForObject(buildUri("stream"), ADNPostsList.class);
+    }
+
+    @Override
+    public List<ADNPost> getUserCreated(String userId) {
+        return restTemplate.getForObject(buildUsersPostsUri(userId + "/posts"), ADNPostsList.class);
+    }
+
+    @Override
+    public List<ADNPost> getUserMentions(String userId) {
+        return restTemplate.getForObject(buildUsersPostsUri(userId + "/mentions"), ADNPostsList.class);
+    }
+
+    // TODO Arikg: remove when the endpoint changes or find a more elegant solution
+    private String buildUsersPostsUri(String uri) {
+        return URIBuilder.fromUri(userPostsBaseURl + uri).queryParam("access_token", accessToken).build().toString();
+    }
+
+    @Override
+    public List<ADNPost> getGlobalStream() {
+        return restTemplate.getForObject(buildUri("stream/global"), ADNPostsList.class);
+    }
+
+    @Override
+    public List<ADNPost> getHashtagStream(String hashtag) {
+        return restTemplate.getForObject(buildUri("tag/" + hashtag), ADNPostsList.class);
+    }
+
+    @Override
+    public List<ADNPost> getPostReplies(String id) {
+        return restTemplate.getForObject(buildUri(id + "/replies"), ADNPostsList.class);
     }
 }
